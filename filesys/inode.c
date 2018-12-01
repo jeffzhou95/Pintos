d47f8251f0215379be59d5b85c87dd4356bcc2bd
jeffzhou95 <xz6429@utexas.edu>
2018-12-01 14:01:43 -0600

    file growth

--- filesys/inode.c
+++ filesys/inode.c
@@ -31,6 +31,7 @@ struct inode_indirect_block_sector {
   block_sector_t blocks[INDIRECT_BLOCKS_PER_SECTOR];
 };
 static bool inode_allocate (struct inode_disk *disk_inode);
+static bool inode_reserve (struct inode_disk *disk_inode, off_t length);
 static bool inode_deallocate (struct inode *inode);
 
 /* Returns the number of sectors to allocate for an inode SIZE
@@ -320,8 +321,14 @@ inode_write_at (struct inode *inode, const void *buffer_, off_t size,
     return 0;
 
   // TODO: extensible file system here
-  if( byte_to_sector(inode, offset + size - 1) == -1 ) {
-    PANIC ("Extensible file system is not implemented");
+  if( byte_to_sector(inode, offset + size - 1) == -1u ) {
+    // extend and reserve up to [offset + size] bytes
+    bool success;
+    success = inode_reserve (& inode->data, offset + size);
+    if (!success) return 0;  // fail?
+    // write back the (extended) file size
+    inode->data.length = offset + size;
+    buffer_cache_write (inode->sector, & inode->data);
   }
 
 
@@ -405,17 +412,25 @@ inode_length (const struct inode *inode)
   return inode->data.length;
 }
 
+static
+bool inode_allocate (struct inode_disk *disk_inode)
+{
+  return inode_reserve (disk_inode, disk_inode->length);
+}
+
 
 static void
-inode_allocate_indirect (block_sector_t* p_entry, size_t num_sectors, int level)
+inode_reserve_indirect (block_sector_t* p_entry, size_t num_sectors, int level)
 {
   static char zeros[BLOCK_SECTOR_SIZE];
   // only supports 2-level indirect block scheme as of now
   ASSERT (level <= 2);
   if (level == 0) {
     // base case : allocate a single sector and put it into the block
-    free_map_allocate (1, p_entry);
-    buffer_cache_write (*p_entry, zeros);
+    if (*p_entry == 0) {
+      free_map_allocate (1, p_entry);
+      buffer_cache_write (*p_entry, zeros);
+    }
     return;
   }
   struct inode_indirect_block_sector indirect_block;
@@ -429,37 +444,39 @@ inode_allocate_indirect (block_sector_t* p_entry, size_t num_sectors, int level)
   size_t i, l = DIV_ROUND_UP (num_sectors, unit);
   for (i = 0; i < l; ++ i) {
     size_t subsize = min(num_sectors, unit);
-    inode_allocate_indirect (& indirect_block.blocks[i], subsize, level - 1);
+    inode_reserve_indirect (& indirect_block.blocks[i], subsize, level - 1);
     num_sectors -= subsize;
   }
   ASSERT (num_sectors == 0);
   buffer_cache_write (*p_entry, &indirect_block);
 }
-static
-bool inode_allocate (struct inode_disk *disk_inode)
+
+static bool
+inode_reserve (struct inode_disk *disk_inode, off_t length)
 {
   static char zeros[BLOCK_SECTOR_SIZE];
-  off_t file_length = disk_inode->length;
-  if(file_length < 0) return false;
+  if (length < 0) return false;
   // (remaining) number of sectors, occupied by this file.
-  size_t num_sectors = bytes_to_sectors(file_length);
+  size_t num_sectors = bytes_to_sectors(length);
   size_t i, l;
   // (1) direct blocks
   l = min(num_sectors, DIRECT_BLOCKS_COUNT * 1);
   for (i = 0; i < l; ++ i) {
-    free_map_allocate (1, &disk_inode->direct_blocks[i]);
-    buffer_cache_write (disk_inode->direct_blocks[i], zeros);
+    if (disk_inode->direct_blocks[i] == 0) {
+      free_map_allocate (1, &disk_inode->direct_blocks[i]);
+      buffer_cache_write (disk_inode->direct_blocks[i], zeros);
+    }
   }
   num_sectors -= l;
   if(num_sectors == 0) return true;
   // (2) a single indirect block
   l = min(num_sectors, 1 * INDIRECT_BLOCKS_PER_SECTOR);
-  inode_allocate_indirect (& disk_inode->indirect_block, l, 1);
+  inode_reserve_indirect (& disk_inode->indirect_block, l, 1);
   num_sectors -= l;
   if(num_sectors == 0) return true;
   // (3) a single doubly indirect block
   l = min(num_sectors, 1 * INDIRECT_BLOCKS_PER_SECTOR * INDIRECT_BLOCKS_PER_SECTOR);
-  inode_allocate_indirect (& disk_inode->doubly_indirect_block, l, 2);
+  inode_reserve_indirect (& disk_inode->doubly_indirect_block, l, 2);
   num_sectors -= l;
   if(num_sectors == 0) return true;
   ASSERT (num_sectors == 0);
